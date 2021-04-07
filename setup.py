85a97256a635a502bd45e8bf01affe00763ade77
KiraPC <pasqualecarmine.carbone@gmail.com>
2021-04-07 17:42:50 +0200

    testing release cd

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ with open('README.md', 'r') as fh:
 with open('requirements.txt', 'r') as fin:
     REQS = fin.read().splitlines()
 
-__VERSION__ = '0.2.5'
+VERSION = '__VERSION__'
 
 setuptools.setup(
-    version=__VERSION__,
+    version=VERSION,
     name='fastapi-router-controller',
     author='Pasquale Carmine Carbone',
     author_email='pasqualecarmine.carbone@gmail.com',
