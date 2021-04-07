4a61aa3fb23e7b82b7bf28dd06183c8f7732512a
KiraPC <pasqualecarmine.carbone@gmail.com>
2021-04-07 14:17:29 +0200

    Fix after merge

--- example/sample_app/app.py
+++ example/sample_app/app.py
@@ -1,4 +1,3 @@
-import unittest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from fastapi_router_controller import ControllersTags
@@ -12,18 +11,3 @@ app = FastAPI(
 
 app.include_router(SampleController.router())
 app.include_router(AnotherSampleController.router())
-
-class TestRoutes(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
-
-    def test_get_sample_controller(self):
-        response = self.client.get("/sample_controller/?id=1234")
-        self.assertEqual(response.status_code, 200)
-
-    def test_post_sample_controller(self):
-        response = self.client.post("/sample_controller/", json={"id": "test"})
-        self.assertEqual(response.status_code, 201)
-
-if __name__ == "__main__":
-    unittest.main()
--- example/sample_app_with_auto_import/app.py
+++ example/sample_app_with_auto_import/app.py
@@ -1,6 +1,4 @@
-import unittest
 from fastapi import FastAPI
-from fastapi.testclient import TestClient
 from fastapi_router_controller import Controller, ControllersTags
 
 # just import the main package to load all the controllers in
@@ -14,18 +12,3 @@ app = FastAPI(
 
 for router in Controller.routers():
     app.include_router(router)
-
-class TestRoutes(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
-
-    def test_get_sample_extended_controller(self):
-        response = self.client.get("/sample_extended_controller/?id=1234")
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_sample_extended_controller_parent_api(self):
-        response = self.client.get("/sample_extended_controller/parent_api")
-        self.assertEqual(response.status_code, 200)
-
-if __name__ == "__main__":
-    unittest.main()
