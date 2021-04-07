d5f01f3978acdb26759258eaafff91debce4c200
Josip Delic <delijati@googlemail.com>
2021-04-07 12:13:53 +0200

    Set version

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 
     Setup module
 '''
-import os
 import setuptools
 
 with open('README.md', 'r') as fh:
@@ -13,8 +12,10 @@ with open('README.md', 'r') as fh:
 with open('requirements.txt', 'r') as fin:
     REQS = fin.read().splitlines()
 
+VERSION = "0.2.5"
+
 setuptools.setup(
-    version=os.environ.get('RELEASE_VERSION'),
+    version=VERSION,
     name='fastapi-router-controller',
     author='Pasquale Carmine Carbone',
     author_email='pasqualecarmine.carbone@gmail.com',
